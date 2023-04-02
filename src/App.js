aebf74b23621f02b9806789a9c3a1392c1f1b021
dhanushwaran06 <ajdhanush.jvl@gmail.com>
2023-04-02 18:07:54 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import SimpleRestProvider from "ra-data-simple-rest";
+import postList from "./components/postList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Admin dataProvider={SimpleRestProvider("http://localhost:3000")}>
+    <Resource name="post" list={postList} />
+  </Admin>
+);
 
 export default App;
